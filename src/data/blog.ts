fe5d7014511e0b8676c6ce1c91a556cd523982ef
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-07-21 13:08:23 +0530

    blogs coming soon

---
+++ src/data/blog.ts
@@ -0,0 +1,4 @@
+export const blogData = {
+  title: `Shweta Labs Blogs`,
+  description: `Our blogs from our founders, analysts and experts analyze the latest developments in cyber-security, providing a variety of perspectives on this intriguing, exciting and challenging world. There are multiple lenses to see threats through and a single-minded aim – how to eliminate them.`,
+};
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,3 +1,20 @@
+import { ComingSoon } from '../components/misc';
+import { blogData } from '../data/blog';
+
 export function Blog(): JSX.Element {
-  return <div>Blog</div>;
+  return (
+    <div
+      className='col'
+      style={{ width: '55vw', marginInline: 'auto', alignItems: 'center' }}
+    >
+      <p className='title2 fs-x-large'>{blogData.title}</p>
+      <p
+        className='shady-70 centered'
+        style={{ width: '45vw', marginTop: '2.4vw' }}
+      >
+        {blogData.description}
+      </p>
+      <ComingSoon />
+    </div>
+  );
 }
