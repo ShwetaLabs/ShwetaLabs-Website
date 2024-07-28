2ea630cc7fb522ffc02443c652b846acd0f8b93e
SHWETALABS <shwetalabspvtltd@gmail.com>
2024-07-28 19:40:27 +0530

    Home Page Content Edit Beside SL Logo

--- src/data/home.ts
+++ src/data/home.ts
@@ -4,7 +4,7 @@ import { contactLink } from './navigation';
 export const advertData = {
   title: 'For Enforcement <br> and Enforcers',
   description: `Shweta Labs Cyber-Security tools are made for law enforcement agencies - keeping the end user - the investigating officer or “enforcer” - in mind. Our tools are easy-to-use and “enforcer-friendly.”`,
-  descriptionBesideLogo: `Our belief is that threats of the future will be in cyberspace. Be it terrorism, trafficking, fraud or information warfare. Our services work proactively to discover threats before they happen; or reactively, in eliminating them as they occur.`,
+  descriptionBesideLogo: `Our services coalesce into a single point - designed to meet all Law Enforcement Needs in Cyber Security. Our products and services are meant to support requirements as diverse as tracking suspects and profiling perpetrators. In other words – we aim to be a holistic, all-encompassing cyber support for our clients and customers.`,
   logo: images.footerLogo,
 };
 
