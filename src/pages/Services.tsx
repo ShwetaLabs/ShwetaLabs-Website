7eb48a06067bbebece0c90f3559198d9417bc4f0
Om Thakare <omthakare1606@gmail.com>
2024-11-23 16:15:52 +0530

    updated the width for scope paragraph

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -5,9 +5,9 @@ import { isDesktop } from '../utils';
 export function Services(): JSX.Element {
   return isDesktop() ? (
     <div style={{ marginLeft: '160px', marginRight: '160px' }}>
-      <div style={{ marginLeft: '192px', marginRight: '192px' }}>
+      <div style={{ marginInline:"120px", display:"flex", flexDirection:"column", alignItems:"center" }}>
         <p className='title2 fs-x-large'>{servicesData.title}</p>
-        <p className='shady-70 centered'>{servicesData.subtitle}</p>
+        <p className='shady-70 centered' style={{}}>{servicesData.subtitle}</p>
       </div>
       {servicesData.capababilites.map((it, index) => (
         <div style={{ margin: '30px',marginBottom: '50px'  }}>
